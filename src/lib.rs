44cc6f44daadcfc73bef5064bb17c5876a068045
Shoan Chikato <sjchikato@gmail.com>
2024-06-20 21:50:31 +0200

    add post repo

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod fun;
 pub mod model;
+pub mod repo;
---
+++ src/repo/post.rs
@@ -0,0 +1,54 @@
+use crate::model::post::Post;
+
+pub struct PostRepo {
+    posts: Vec<Post>,
+}
+
+impl PostRepo {
+    pub fn new() -> PostRepo {
+        PostRepo { posts: vec![] }
+    }
+
+    pub fn get_all(&self) -> &Vec<Post> {
+        &self.posts
+    }
+
+    pub fn add(&mut self, post: Post) {
+        self.posts.push(post);
+    }
+
+    pub fn remove(&mut self, id: usize) {
+        match self.posts.get(id) {
+            Some(_) => {
+                self.posts.remove(id);
+            }
+            None => {
+                eprint!("Post with id {}, not found", id);
+                return;
+            }
+        }
+    }
+
+    pub fn edit(&mut self, id: usize, post: &Post) {
+        match self.posts.get_mut(id) {
+            Some(old_post) => {
+                old_post.title = if post.title.clone().is_empty() {
+                    old_post.title.clone()
+                } else {
+                    post.title.clone()
+                };
+
+                old_post.body = if post.body.clone().is_empty() {
+                    old_post.body.clone()
+                } else {
+                    post.body.clone()
+                };
+            }
+
+            None => {
+                eprint!("Post with id {}, not found", id);
+                return;
+            }
+        }
+    }
+}
