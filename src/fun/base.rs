74a0dfd443d77df7be6fc1a9da0f2f389e0cac5a
Shoan Chikato <sjchikato@gmail.com>
2024-06-27 16:53:22 +0200

    add traits for base functionality

--- src/fun/base.rs
+++ src/fun/base.rs
@@ -1,43 +1,65 @@
 use std::io::{stdin, stdout, Write, BufRead, BufReader};
 
-pub fn write_to_string<W: Write>(writer: &mut W, text: &str) {
-    match writer.write(format!("{text}\n").as_bytes()) {
-        Ok(_) => {}
-        Err(_) => {
-            eprintln!("Error writing output");
-            return;
-        }
-    };
-
-    match writer.flush() {
-        Ok(_) => {}
-        Err(_) => {
-            eprintln!("Error flushing message");
-            return;
-        }
-    };
+pub trait StringReadWrite {
+    fn write_to_string(&mut self, text: &str);
+    fn read_from_string(&mut self, text: &mut String);
 }
 
-pub fn read_from_string<R: BufRead>(reader: &mut R, mut text: &mut String) {
-    match reader.read_line(&mut text) {
-        Ok(_) => {}
-        Err(_) => {
-            eprintln!("Error reading input");
-            return;
-        }
-    };
+pub struct StringRW<R: BufRead, W: Write> {
+    reader: R,
+    writer: W,
 }
+
+impl <R: BufRead, W: Write> StringRW<R, W> {
+    fn new(reader: R, writer: W) -> Self {
+        StringRW { reader, writer }
+    }
+}
+
+impl <R: BufRead, W: Write> StringReadWrite for StringRW<R, W> {
+    
+    fn write_to_string(&mut self, text: &str) {
+        match self.writer.write(format!("{text}\n").as_bytes()) {
+            Ok(_) => {}
+            Err(_) => {
+                eprintln!("Error writing output");
+                return;
+            }
+        };
+    
+        match self.writer.flush() {
+            Ok(_) => {}
+            Err(_) => {
+                eprintln!("Error flushing message");
+                return;
+            }
+        };
+    }
+
+    fn read_from_string(&mut self, text: &mut String) {
+        match self.reader.read_line(text) {
+            Ok(_) => {}
+            Err(_) => {
+                eprintln!("Error reading input");
+                return;
+            }
+        };
+    }
+}
+
 
 pub fn get_response(question: &str) -> String {
     let mut input = String::new();
     input.clear();
 
-    let mut stdout = stdout();
+    let stdout = stdout();
     let stdin = stdin();
-    let mut stdin_reader = BufReader::new(stdin.lock());
+    let stdin_reader = BufReader::new(stdin.lock());
 
-    write_to_string(&mut stdout, question);
-    read_from_string(&mut stdin_reader, &mut input);
+    let mut string_io = StringRW::new(stdin_reader, stdout);
+
+    string_io.write_to_string(question);
+    string_io.read_from_string(&mut input);
 
     input.trim().to_string()
 }
