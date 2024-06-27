d4dc652da3865521093e5c0a29040838ce533dd0
Shoan Chikato <sjchikato@gmail.com>
2024-06-27 16:55:02 +0200

    add traits for post repository

--- src/repo/post.rs
+++ src/repo/post.rs
@@ -2,6 +2,14 @@ use std::mem;
 
 use crate::model::post::Post;
 
+pub trait PostIO {
+    fn get_all(&self) -> &Vec<Post>;
+    fn get_one(&mut self, index: usize) -> Option<&mut Post>;
+    fn add(&mut self, post: Post);
+    fn remove(&mut self, id: usize);
+    fn edit(&mut self, id: usize, title: &str, body: &str);
+}
+
 pub struct PostRepo {
     posts: Vec<Post>,
 }
@@ -10,20 +18,23 @@ impl PostRepo {
     pub fn new() -> PostRepo {
         PostRepo { posts: vec![] }
     }
+}
 
-    pub fn get_all(&self) -> &Vec<Post> {
+impl PostIO for PostRepo {
+
+    fn get_all(&self) -> &Vec<Post> {
         &self.posts
     }
 
-    pub fn get_one(&mut self, index: usize) -> Option<&mut Post> {
+    fn get_one(&mut self, index: usize) -> Option<&mut Post> {
         self.posts.get_mut(index)
     }
 
-    pub fn add(&mut self, post: Post) {
+    fn add(&mut self, post: Post) {
         self.posts.push(post);
     }
 
-    pub fn remove(&mut self, id: usize) {
+    fn remove(&mut self, id: usize) {
         match self.posts.iter().position(|post| post.id == id) {
             Some(index) => {
                 self.posts.remove(index);
@@ -35,7 +46,7 @@ impl PostRepo {
         }
     }
 
-    pub fn edit(&mut self, id: usize, title: &str, body: &str) {
+    fn edit(&mut self, id: usize, title: &str, body: &str) {
         match self.posts.iter_mut().find(|post| post.id == id) {
             Some(post) => {
                 post.title = if title.is_empty() {
